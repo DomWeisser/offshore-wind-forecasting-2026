d41c84027c10ee4dbb6f1f940134e21b5d220846
DomWeisser <dweisser99@gmail.com>
2026-01-22 12:46:51 +0100

    Delete src directory

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Wind power forecasting package.
-
-This package contains modules for:
-- Data loading and preprocessing
-- VAE models for feature extraction
-- Clustering algorithms
-- GP-based forecasting
-"""
